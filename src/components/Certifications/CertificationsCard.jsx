651c05779dede7efeb904b8da897c0fe4382ed89
YashSharma2129 <yash25578@gmail.com>
2026-02-12 08:51:41 +0530

    fix build

--- src/components/Certifications/CertificationsCard.jsx
+++ src/components/Certifications/CertificationsCard.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import { CgWebsite } from "react-icons/cg";
-import { BsGithub } from "react-icons/bs";
 import { BsFillAwardFill } from "react-icons/bs";
 
 function CertificationCard(props) {
